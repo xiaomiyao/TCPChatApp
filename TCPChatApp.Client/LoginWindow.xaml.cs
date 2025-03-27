36b250f3ad5cb4e3a60a1d172ac6828694e905f5
xiaomiyao <chantseva123@gmail.com>
2025-03-27 19:12:04 +0100

    added nonfunctional login window

---
+++ TCPChatApp.Client/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace TCPChatApp.Client
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Password;
+
+            // TODO: Send registration request to the server
+            //       Validate response, show success/failure message
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Password;
+
+            // TODO: Send login request to the server
+            //       If success: open main chat window
+            //       If fail: show error message
+            MainWindow chatWindow = new MainWindow();
+            chatWindow.Show();
+
+            // Close the login window
+            this.Close();
+        }
+    }
+}
